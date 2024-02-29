24f9eee9ac0c7c9d29159fd21de4ddf93e8a079f
Chaz-Adams <caadams1922@gmail.com>
2024-02-29 18:23:11 -0500

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer in Jan 2024
@@ -5,23 +7,38 @@
 public class Main {
 
     public static void main(String[] args){
-        
+
+
         int number = getInputNumber();
         
         int sum = sumOfNumbers(number);
+        int sum1 = gaussianSumOfNumbers(number);
+
         
-        System.out.println(sum);
+        System.out.println("Using the for loop, sum: "+sum);
+        System.out.println("Using the Gaussian method, sum: "+sum1);
+
+
 
     }
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    public static int sumOfNumbers(int n) {
+        long startTime, endTime, elapsedTime;
+        startTime = System.nanoTime();
+        int total = 0;
+
+        for(int i = 1; i <= n; i++){
+            total+= i;
+        }
+        endTime = System.nanoTime();
+        elapsedTime = endTime - startTime;
+        System.out.println("Using the for loop took: "+elapsedTime+" nanoseconds");
+        return total;
     }
-    
-    
-    int getInputNumber() { // gee, may thi sis useful in another lab?
+
+    public static int getInputNumber() { // gee, may thi sis useful in another lab?
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
@@ -32,12 +49,16 @@ public class Main {
         scan.close();
         return num;
     }
-    
-    
-    
+
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
+    public static int gaussianSumOfNumbers(int n) {
+        long startTime, endTime, elapsedTime;
+        startTime = System.nanoTime();
+        double total = .5*n*(n+1);
+        endTime = System.nanoTime();
+        elapsedTime = endTime - startTime;
+        System.out.println("Using the Gaussian method took: "+elapsedTime+" nanoseconds");
+        return (int) total;
     }
    
     // for Extra extra credit - compare the methods and show which one is faster
